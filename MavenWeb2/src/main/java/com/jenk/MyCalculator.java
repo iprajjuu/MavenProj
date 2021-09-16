fdbcebfb7d2b0cc8e18716b4f05a86fad6ccc172
Prajna <Prajna@DESKTOP-7M4E3AI.bbrouter>
2021-09-16 12:58:25 -0700

    MavenPrj

---
+++ MavenWeb2/src/main/java/com/jenk/MyCalculator.java
@@ -0,0 +1,14 @@
+package com.jenk;
+
+public class MyCalculator 
+{
+	public int getSum(int x,int y)
+	{
+		return x+y;
+	}
+	public int getDiff(int x,int y)
+	{
+		return x-y;
+	}
+
+}
---
+++ MavenWeb2/src/test/java/MyCalculatorTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+import com.jenk.MyCalculator;
+import org.junit.Test;
+
+public class MyCalculatorTest {
+
+	@Test
+	public void test1() {
+		assertEquals(10, new MyCalculator().getSum(5,5));
+	}
+	@
+	Test
+	public void test2() {
+		assertEquals(5, new MyCalculator().getSum(2,3));
+	}
+	@Test
+	public void test3() {
+		assertEquals(5, new MyCalculator().getSum(10,5));
+	}
+	@Test
+	public void test4() {
+		assertEquals(0, new MyCalculator().getSum(5,5));
+	}
+
+}
